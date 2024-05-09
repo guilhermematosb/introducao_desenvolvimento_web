af12152c7eec215538e83308b04b6dacc1f40707
Guilherme <guilhermematosb@gmail.com>
2024-05-09 19:07:40 -0300

    Retirando descrição desnecessária

--- codigos_js/introducao_javascript/js/manipulando_dados.js
+++ codigos_js/introducao_javascript/js/manipulando_dados.js
@@ -1,11 +1,4 @@
 /* 
-    O JavaScript possui uma característica chamada Prototype, que é uma série de funcionalidades 
-    e atributos que já existem nativamente em diversos tipos de dados, que são lidados como objetos.
-*/
-
-/* 
-    Type conversion (typecasting) vs Type coersion
-
     Type conversion se trata de uma conversão de tipo feita pelo dev, explicitamente, 
     e pode ser feita de diversas maneiras, por exemplo com o método Number(), 
     já type coersion é uma mudança de tipos feita pelo próprio JavaScript, implicitamente, 
